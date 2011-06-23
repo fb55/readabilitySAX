58dd241268b8aded3bd81a942d92436f2d9e3dda
Felix Böhm <me@feedic.com>
2011-06-23 17:17:06 +0200

    added an example for incremental search for content on a page

--- node examples/mkread.js
+++ node examples/mkread.js
@@ -2,14 +2,8 @@ console.log("connecting to:", process.argv[2]);
 
 var sax =  require('../libs/sax'),
 	readability = require("../readabilitysax"),
-	url = require("url");
-
-
-
-var link = url.parse(process.argv[2]),
-	client = require("http").createClient(80, link.host),
-	req = client.request("GET", link.href, { 'host': link.host, 'accept':"text/html" }),
-	data = "";
+	url = require("url"),
+	link = "";
 
 var parserSettings = {
 	trim : true,
@@ -20,36 +14,47 @@ var convertLinks = function(a){
 	return url.resolve(link, a);
 };
 
+var processContent = function(data){
+	var conTime = Date.now(),
+		skipLevel = 0,
+		contentLength = 0,
+		parser, readable, ret;
+	
+	while(contentLength < 250 && skipLevel < 4){
+	    parser = sax.parser(false, parserSettings);
+	    
+	    readable = new readability.process(parser, {
+	    	convertLinks: convertLinks,
+	    	pageURL: url.format(link)
+	    });
+	    
+	    parser.write(data).close();
+	    
+	    ret = readable.getArticle();
+	    contentLength = ret.textLength;
+	    skipLevel += 1;
+	}
+	
+	ret.skipLevel = skipLevel - 1;
+	ret.duration = Date.now() - conTime;
+	console.log(ret);
+}
+
+//processContent(require("fs").readFileSync(__dirname + "/testfile.html") + "");
+
+link = url.parse(process.argv[2]);
+
+var client = require("http").createClient(80, link.host),
+	req = client.request("GET", link.href, { 'host': link.host, 'accept':"text/html" }),
+	data = "";
+
 req.addListener("response", function(connection){
 	connection.addListener("data", function(chunk){
 		//parser.write(chunk.toString("utf8"));
 		data += chunk.toString("utf8");
 	});
 	connection.addListener("end", function(){
-		var conTime = Date.now();
-		var skipLevel = 0;
-		var contentLength = 0;
-		var parser, readable, ret;
-		
-		while(contentLength < 250 && skipLevel < 4){
-			parser = sax.parser(false, parserSettings);
-			
-			readable = new readability.process(parser, {
-				convertLinks: convertLinks,
-				pageURL: url.format(link)
-			});
-	    	
-	    	parser.write(data).close();
-	    	
-	    	ret = readable.getArticle();
-	    	contentLength = ret.textLength;
-	    	skipLevel += 1;
-	    }
-		
-		ret.skipLevel = skipLevel - 1;
-		ret.duration = Date.now() - conTime;
-		console.log(ret);
+		processContent(data);
 	});
 });
 req.end();
-//parser.write(require("fs").readFileSync(__dirname + "/testfile.html") + "").close();
