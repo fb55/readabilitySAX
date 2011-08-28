6323c73bf6f9af66c2a5ed7e5ef33ca9884e9de5
Felix Böhm <me@feedic.com>
2011-08-28 20:08:20 +0200

    Added support for htmlparser2, readabilitySAX should now support most websites

--- node_examples/getReadableContent.js
+++ node_examples/getReadableContent.js
@@ -1,91 +1,123 @@
-var sax =  require("sax"),
-	readability = require("../readabilitySAX"),
+var readability = require("../readabilitySAX"),
+	request = require("request"),
 	url = require("url");
 
-exports.get = function(uri, cb){
-	if(!uri || uri.trim() === ""){cb({
-			title:	"Error",
-	    	text:	"No URI specified!",
-	    	html:	"<b>No URI specified!</b>",
-	    	error: true
-	    });
-	    return;
+var sax, htmlparser, EventedHandler;
+try{ sax = require("sax"); } catch(e){}
+try{
+	EventedHandler = require("htmlparser2/lib/EventedHandler.js");
+	htmlparser = require("htmlparser2/lib/Parser.js");
+} catch(e){}
+
+
+/*
+* function getParser abstracts the parser
+* it returns an object including readability
+* and the parsers write method
+*/
+function getParser(settings, slowParser){
+	if((slowParser && htmlparser && EventedHandler) || !sax){ //if the sax parser isn't present, don't rely on it
+		var cbs = {},
+			readable = readability.process(cbs, settings),
+			handler = new EventedHandler(cbs),
+			parser = new htmlparser(handler);
+		
+		return {
+			write: parser.parseChunk.bind(parser),
+			close: parser.done.bind(parser),
+			getArticle: readable.getArticle.bind(readable)
+		}
 	}
-	
-	var link = url.parse(uri),
-		client = require("http").createClient(80, link.host),
-		req = client.request("GET", link.href, { host: link.host, accept:"text/html" });
-	
-	client.addListener("error", function(err) {
-		cb({
-			title:	"Error",
-	    	text:	"Couldn't find uri!",
-	    	html:	"<b>Couldn't find uri!</b>",
-	    	error: true
-	    });
-	});
-	
-	req.addListener("response", function(connection){
-		if(connection.statusCode === 301 || connection.statusCode === 302)
-			return exports.get(connection.headers.location, cb);
+	else {
 		var parser = sax.parser(false, {
 		    lowercasetags : true
 		});
-		var data = "";
+		var readable = new readability.process(parser, settings);
 		
-		var readable = new readability.process(parser, {
-			convertLinks: function(a){
-				return url.resolve(link, a);
-			},
-			pageURL: url.format(link)
+		return {
+			write: parser.write.bind(parser),
+			close: parser.close.bind(parser),
+			getArticle: readable.getArticle.bind(readable)
+		}
+	}
+}
+
+exports.get = function(uri, cb, options){
+	options = options || {slowParser: true};
+	
+	function onErr(err){
+		cb({
+			title:	"Error",
+	    	text:	err.toString(),
+	    	html:	"<b>" + err.toString() + "</b>",
+	    	error: true
+	    });
+	}
+	
+	if(!uri || uri.trim() === ""){
+	    return onErr("No URI specified!");
+	}
+	
+	var link = url.parse(uri),
+		parser, data = "", settings,
+		onResponseCB = function(err, resp){
+			if(err) return onErr(err);
+			
+			link = resp.request.uri;
+			
+			settings = {
+				convertLinks: url.resolve.bind(null, link),
+				link: link
+			};
+			
+			parser = getParser(settings, options.slowParser);
+		},
+		req = request({
+			uri: link,
+			onResponse: onResponseCB
 		});
+	
+	req.on("error", onErr);
+	
+	req.on("data", function(chunk){ chunk = chunk.toString("utf8"); parser.write(chunk); data += chunk; })
+	
+	req.on("end", function(){
+		parser.close();
 		
-		connection.on("data", function(chunk){
-			data += chunk = chunk.toString("utf8");
-			parser.write(chunk);
-		});
-		connection.on("end", function(){
-			parser.close();
-			var ret = readable.getArticle();
-			if(ret.textLength < 250){
-				ret = exports.process(data, 1, {
-					convertLinks: url.resolve.bind(null, link),
-					link: link
-				});	
-			}
-			ret.link = link.href;
-			cb(ret);
-		});
+		var ret = parser.getArticle();
+		if(ret.textLength < 250){
+			ret = exports.process(data, settings);	
+	    }
+	    ret.link = link.href;
+	    cb(ret);
 	});
-	req.end();
 };
-exports.process = function(data, skipLevel, readabilitySettings, type){
-	skipLevel = skipLevel || 0;
-	readabilitySettings = readabilitySettings || {};
+exports.process = function(data, options){
+	skipLevel = options.skipLevel || 0;
+	readabilitySettings = options.readabilitySettings || {};
 	
 	var contentLength = 0,
-		parser, readable, ret;
+		parser, ret;
 	
 	while(contentLength < 250 && skipLevel < 4){
-	    parser = sax.parser(false, {
-		    lowercasetags : true
-		});
-	    
 	    readabilitySettings.skipLevel = skipLevel;
 	    
-	    readable = new readability.process(parser, readabilitySettings);
+	    parser = getParser(readabilitySettings, options.slowParser);
 	    
-	    parser.write(data).close();
+	    parser.write(data);
+	    parser.close();
 	    
-	    ret = readable.getArticle(type);
+	    ret = parser.getArticle(options.type);
 	    contentLength = ret.textLength;
 	    skipLevel += 1;
 	}
+	/*
 	if(contentLength < 250) return {
 			title:	"Error",
 	    	text:	"Couldn't find content!",
 	    	html:	"<b>Couldn't find content!</b>",
 	    	error: true
 	};
-	else return ret;
+	*/
+	return ret;
 };
--- node_examples/mkread.js
+++ node_examples/mkread.js
@@ -1,37 +1,35 @@
 var getReadableContent = require("./getReadableContent.js"),
-	url = require("url"),
-	link = "";
+	request = require("request"),
+	url = require("url");
 
 var processContent = function(data){
 	var conTime = Date.now();
 	
-	var ret = getReadableContent.process(data, 0, {
-		convertLinks: url.resolve.bind(null, link),
-	    link: link
+	var settings = link ? {
+			convertLinks: url.resolve.bind(null, link),
+		 	link: link
+		 } : {};
+	
+	var ret = getReadableContent.process(data, {
+		skipLevel: 0,
+		readabilitySettings: settings,
+		 slowParser: true
 	});
 	
 	ret.duration = Date.now() - conTime;
 	console.log(ret);
 }
 
-
 if(process.argv.length > 2){
 	console.log("connecting to:", process.argv[2]);
+	
 	var link = url.parse(process.argv[2]);
 	
-	var client = require("http").createClient(80, link.host),
-		req = client.request("GET", link.href, { 'host': link.host, 'accept':"text/html" }),
-		data = "";
-	
-	req.addListener("response", function(connection){
-		connection.addListener("data", function(chunk){
-			data += chunk.toString("utf8");
-		});
-		connection.addListener("end", function(){
-			processContent(data);
-		});
+	request({uri:link}, function(err, resp){
+		link = resp.request.uri;
+		console.log(link);
+		processContent(resp.body);
 	});
-	req.end();
 }
 else
 	require("fs").readFile(__dirname + "/testpage.html", function(a,b){processContent(b.toString("utf8"));});
