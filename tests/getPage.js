e792c58ac50d1feaaf87b3eab500217994301608
Felix Böhm <me@feedic.com>
2012-01-07 16:46:44 +0100

    CLI/getPage now returns text per default

--- tests/getPage.js
+++ tests/getPage.js
@@ -1,1 +1,22 @@
-require("../").get(process.argv[2], console.log);
+if(process.argv.length < 3 || !/^https?:\/\//.test(process.argv[2])){
+	console.log("Usage:", "readability", "http://domain.tld/sub");
+	return;
+}
+
+require("minreq")(process.argv[2])
+.on("error", console.log.bind(null, "Request error:"))
+.pipe(new (require("../node/stream.js"))({
+	type: process.argv[3] || "text" //default output is text
+}))
+.on("error", console.log.bind(null, "Parsing error:"))
+.on("data", function(result){
+	console.log("TITLE:", result.title);
+	console.log("URL:", result.link);
+	console.log("SCORE:", result.score);
+	if(result.nextPage) console.log("NEXT PAGE:", result.nextPage);
+	console.log("LENGTH:", result.textLength);
+	console.log("");
+	console.log(result.text || result.html);
+	
+	process.exit();
+});
