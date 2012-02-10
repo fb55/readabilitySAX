31f4fa08a0cdf1416934e6172f748d1c3a9089dd
Felix Böhm <me@feedic.com>
2012-02-10 15:15:41 +0100

    Use the `entities` module to replace html entities in CLI, added it as an decency

--- node/CLI.js
+++ node/CLI.js
@@ -5,7 +5,7 @@ if(process.argv.length < 3 || !/^https?:\/\//.test(process.argv[2])){
 	return;
 }
 
-require("./getURL.js")(process.argv[2], "text", function(result){
+require("./getURL.js")(process.argv[2], process.argv[3] === "html" ? "html" : "text", function(result){
 	if(result.error) return console.log("Error:", result.text);
 
 	//else
@@ -14,7 +14,14 @@ require("./getURL.js")(process.argv[2], "text", function(result){
 	if(result.nextPage) console.log("NEXT PAGE:", result.nextPage);
 	console.log("LENGTH:", result.textLength);
 	console.log("");
-	console.log(result.text || result.html);
+	
+	var text;
+	if("text" in result){
+		text = require("entities").decodeHTML5(result.text);
+	} else {
+		text = result.html.replace(/\s+/g, " ");
+	}
+	console.log(text);
 	
 	process.exit();
 });
