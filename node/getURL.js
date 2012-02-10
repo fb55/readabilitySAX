f872080442cb368df0f8417cbc59c5f67f6f8b47
Felix Böhm <me@feedic.com>
2012-02-10 15:01:49 +0100

    Moved get and process from index.js to separate files
    Other changes:
    • Use WritableStream in get
    • Export WritableStream
    • Added an optional format parameter to get

---
+++ node/getURL.js
@@ -0,0 +1,48 @@
+var WritableStream = require("./WritableStream.js"),
+	minreq = require("minreq"),
+	url = require("url"),
+	processData = require("./index.js").process;
+
+module.exports = function(uri, format, cb){
+	if(typeof format === "function"){
+		cb = format;
+		format = "html";
+	}
+
+	var calledCB = false;
+	function onErr(err){
+		if(calledCB) return;
+		calledCB = true;
+		
+		err = err.toString();
+		cb({
+			title:	"Error",
+			text:	err,
+			html:	"<b>" + err + "</b>",
+			error: true
+		 });
+	}
+	
+	var settings, stream;
+
+	var req = minreq({
+		uri: typeof uri === "object" ? uri : url.parse(uri),
+		only2xx: true
+	}, function(err, headers, body){
+		if(err) return onErr(err);
+		if(!stream) return onErr("Got no stream!");
+		if(calledCB) return console.log("got end with calledCB = true");
+		
+		var article = stream.getArticle();
+		if(article.textLength < 250 && article.score < 300){
+			article = processData(body, settings, 1);	
+		}
+		
+		article.link = req.response.location;
+		cb(article);
+	}).on("response", function(resp){
+		settings = {pageURL: req.response.location, type: format};
+		stream = new WritableStream(settings);
+		req.pipe(stream);
+	});
+};
---
+++ node/process.js
@@ -0,0 +1,32 @@
+var Readability = require("../readabilitySAX.js"),
+	Parser = require("htmlparser2/lib/Parser.js"),
+	parserOptions = {
+		lowerCaseTags: true
+	};
+
+module.exports = function(data, settings, skipLevel){
+	if(!skipLevel) skipLevel = 0;
+	
+	var readable = new Readability(settings),
+		parser = new Parser(readable, parserOptions),
+		article;
+	
+	do {
+		if(skipLevel !== 0) readable.setSkipLevel(skipLevel);
+
+		 parser.parseComplete(data);
+
+		 article = readable.getArticle();
+		 skipLevel += 1;
+	} while(article.score < 300 && article.textLength < 250 && skipLevel < 4);
+	
+	/*
+	if(article.textLength < 250) return {
+			title:	"Error",
+			text:	"Couldn't find content!",
+			html:	"<b>Couldn't find content!</b>",
+			error: true
+	};
+	*/
+	return article;
+};
