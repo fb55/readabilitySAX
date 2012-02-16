60b84f1317c115d0f006c8e04e84ebbd9aa14260
Felix Böhm <me@feedic.com>
2012-02-16 16:48:10 +0100

    Improved CLI - removed console.log, use own function

--- node/CLI.js
+++ node/CLI.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
+function write(){
+	process.stdout.write(Array.prototype.join.call(arguments, " ") + "\n");
+};
+
 if(process.argv.length < 3 || !/^https?:\/\//.test(process.argv[2])){
-	console.log("Usage:", "readability", "http://domain.tld/sub");
+	write("Usage:", "readability", "http://domain.tld/sub", "[format]");
 	return;
 }
 
 require("./getURL.js")(process.argv[2], process.argv[3] === "html" ? "html" : "text", function(result){
-	if(result.error) return console.log("Error:", result.text);
+	if(result.error) return write("ERROR:", result.text);
 
 	//else
-	console.log("TITLE:", result.title);
-	console.log("SCORE:", result.score);
-	if(result.nextPage) console.log("NEXT PAGE:", result.nextPage);
-	console.log("LENGTH:", result.textLength);
-	console.log("");
+	write("TITLE:", result.title);
+	write("SCORE:", result.score);
+	if(result.nextPage) write("NEXT PAGE:", result.nextPage);
+	write("LENGTH:", result.textLength);
+	write("");
 	
 	var text;
 	if("text" in result){
@@ -21,7 +25,7 @@ require("./getURL.js")(process.argv[2], process.argv[3] === "html" ? "html" : "t
 	} else {
 		text = result.html.replace(/\s+/g, " ");
 	}
-	console.log(text);
+	write(text);
 	
 	process.exit();
 });
