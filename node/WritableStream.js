196795ede5c2a6be5356cb75f9cb73327b5c2f4e
Felix Böhm <me@feedic.com>
2012-09-21 15:01:43 +0200

    extended WritableStream, improved caching

--- node/WritableStream.js
+++ node/WritableStream.js
@@ -4,15 +4,41 @@ var Readability = require("../readabilitySAX.js"),
 		lowerCaseTags: true
 	};
 
-var WritableStream = function(settings){
+var WritableStream = function(settings, callback){
 	Readability.call(this, settings);
 	WritableParser.call(this, this, parserOptions);
+	this._ws_queue = [];
+	this._ws_callback = callback;
 };
 
 require("util").inherits(WritableStream, WritableParser);
 
 Object.getOwnPropertyNames(Readability.prototype).forEach(function(name){
-	WritableStream.prototype[name] = Readability.prototype[name];
+	if(name.substr(0, 2) === "on") {
+		WritableStream.prototype[name] = function(){
+			this._ws_queue.push(name, arguments);
+			Readability.prototype[name].apply(this, arguments);
+		};
+	}
+	else {
+		WritableStream.prototype[name] = Readability.prototype[name];
+	}
 });
+
+WritableStream.prototype.onend = function(){
+	for(var candidate, skipLevel = 1;
+		(candidate = this._getCandidateNode()).info.textLength < 250 &&
+		skipLevel < 4;
+	skipLevel++){
+		this.reset();
+		this.setSkipLevel(skipLevel);
+
+		for(var i = 0; i < this._ws_queue.length; i+=2){
+			Readability.prototype[this._ws_queue[i]].apply(this, this._ws_queue[i+1]);
+		}
+	}
+
+	this._ws_callback(this.getArticle());
+};
 
 module.exports = WritableStream;
